489ed7ee97da7383d7bbb9529b9aa08290e07945
username <wnt925@gmail.com>
2019-04-17 22:02:40 -0600

    update lion solution

--- initialize/lib/lion.rb
+++ initialize/lib/lion.rb
@@ -2,6 +2,9 @@ class Lion
   attr_accessor :name
   attr_accessor :sound
   def initialize(name)
-    @name = :name.values
+    @name = name.values.first
   end
+  def sound
+    @sound = "roar"
+end
 end
