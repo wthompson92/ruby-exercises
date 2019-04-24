c8be7618f1b51c1eedbca69598087384998f261f
username <wnt925@gmail.com>
2019-04-23 18:55:34 -0600

    add test

--- objects-and-methods/exercise-2/test/bag_test.rb
+++ objects-and-methods/exercise-2/test/bag_test.rb
@@ -10,17 +10,17 @@ class BagTest < Minitest::Test
   end
 
   def test_count_the_candies_in_an_empty_bag
-    skip
+
     assert_equal 0, Bag.new.count
   end
 
   def test_empty_bag_has_no_candies
-    skip
+
     assert_equal [], Bag.new.candies
   end
 
   def test_put_candy_in_the_bag
-    skip
+
     bag = Bag.new
     candy = Candy.new("Sour frogs")
     bag << candy
@@ -28,21 +28,21 @@ class BagTest < Minitest::Test
   end
 
   def test_a_bag_with_candies_is_not_empty
-    skip
+
     bag = Bag.new
     bag << Candy.new("Nerds")
     refute bag.empty?
   end
 
   def test_bag_counts_candies
-    skip
+
     bag = Bag.new
     bag << Candy.new("Caramelized Almonds")
     assert_equal 1, bag.count
   end
 
   def test_bag_contains_candies_and_candies_have_a_type
-    skip
+
     bag = Bag.new
     bag << Candy.new("Hershey's Kisses")
     # You usually don't want to chain a bunch of different
@@ -54,7 +54,7 @@ class BagTest < Minitest::Test
   end
 
   def test_ask_bag_if_it_contains_a_particular_type_of_candy
-    skip
+
     bag = Bag.new
     bag << Candy.new("Lindt chocolate")
 
@@ -63,7 +63,7 @@ class BagTest < Minitest::Test
   end
 
   def test_get_a_particular_type_of_candy
-    skip
+
     bag = Bag.new
     bag << Candy.new("Jawbreaker")
     bag << Candy.new("Jawbreaker")
@@ -74,7 +74,7 @@ class BagTest < Minitest::Test
   end
 
   def test_grabbing_candy_removes_it_from_the_bag
-    skip
+
     bag = Bag.new
     bag << Candy.new("Reese's Pieces")
     bag << Candy.new("Junior Mints")
@@ -86,7 +86,7 @@ class BagTest < Minitest::Test
   end
 
   def test_take_a_number_of_candies_from_the_bag
-    skip
+
     bag = Bag.new
     bag << Candy.new("Swedish Fish")
     bag << Candy.new("Milky Way")
@@ -100,7 +100,7 @@ class BagTest < Minitest::Test
   end
 
   def test_take_one_candy
-    skip
+    
     bag = Bag.new
     bag << Candy.new("Lifesavers")
 
@@ -108,4 +108,3 @@ class BagTest < Minitest::Test
     assert_equal "Lifesavers", candy.first.type
   end
 end
-
--- objects-and-methods/exercise-2/test/candy_test.rb
+++ objects-and-methods/exercise-2/test/candy_test.rb
@@ -10,21 +10,20 @@ class CandyTest < Minitest::Test
   end
 
   def test_other_type_of_candy
-    skip
+
     candy = Candy.new("Mars")
     assert_equal "Mars", candy.type
   end
 
   def test_amount_of_sugar_is_100_by_default
-    skip
+
     candy = Candy.new("Circus Peanuts")
     assert_equal 100, candy.sugar
   end
 
   def test_amount_of_sugar_is_configurable
-    skip
+    
     candy = Candy.new("Pop Rocks", 78)
     assert_equal 78, candy.sugar
   end
 end
-
